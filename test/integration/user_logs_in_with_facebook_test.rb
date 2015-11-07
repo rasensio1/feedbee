4d2c20b7b8f38cb239560c9db345168f3e593cb3
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-07 15:04:27 -0700

    add assertions for log in test

--- test/integration/user_logs_in_with_facebook_test.rb
+++ test/integration/user_logs_in_with_facebook_test.rb
@@ -9,5 +9,11 @@ class UserLogsInWithTwitterTest < ActionDispatch::IntegrationTest
   test "logging in" do
     visit "/"
     assert_equal 200, page.status_code
+
+    click_link "Log In with Facebook"
+    assert_equal '/', current_path 
+
+    assert page.has_content?("Ryan")
+    assert page.has_content?("Asensio")
   end
 end
