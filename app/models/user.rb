70dd9a5289b3cca4391dd7e8e399a7743d1f1e55
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-07 14:46:23 -0700

    creates a user from fb oauth response

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth_hash)
-    byebug
 
     user = find_or_create_by(uid: auth_hash.uid)
 
-    user.update_attributes (
-      oauth_token: auth_hash.credentials.token
+    user.update_attributes(
+      oauth_token: auth_hash.credentials.token,
       name:        auth_hash.info.name
     )
     user.save!
