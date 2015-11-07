747a3c9ad5a9ce2c59a493f9b215d45b05138c35
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-07 14:40:32 -0700

    remove first and last name columns. add oauth token. remove provider

---
+++ db/migrate/20151107204621_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :provider, null: false
+      t.string :uid, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
