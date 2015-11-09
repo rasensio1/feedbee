fabea8c4960f478faf13a3f7b0a3db6379421c76
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-08 19:48:14 -0700

    add google places gem

--- app/controllers/google/autocomplete_controller.rb
+++ app/controllers/google/autocomplete_controller.rb
@@ -1,5 +1,6 @@
 class Google::AutocompleteController < ApplicationController
   def index
+    https://maps.googleapis.com/maps/api/place/autocomplete/output?parameters
     byebug
 
     redirect_to place_path 
