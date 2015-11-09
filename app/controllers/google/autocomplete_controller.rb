ceab136a87c54300ed312a9253c5d29e7a332a3f
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-08 21:26:42 -0700

    remove hurley gem. add google place gem

--- app/controllers/google/autocomplete_controller.rb
+++ app/controllers/google/autocomplete_controller.rb
@@ -1,5 +1,22 @@
 class Google::AutocompleteController < ApplicationController
   def index
+    @client = GooglePlaces::Client.new(API_KEY)
+    @client.predictions_by_input(
+      'San F',
+      lat: 0.0,
+      lng: 0.0,
+      radius: 20000000,
+      types: 'geocode',
+      language: I18n.locale,
+    )
+    
+
+    client = Hurley::Client.new(
+    "https://maps.googleapis.com/maps/api/place/autocomplete/json?parameters")
+
+    client.query["key"] = ENV['GOOGLE_KEY']
+    client.query["input"] = params[:search] 
+
     https://maps.googleapis.com/maps/api/place/autocomplete/output?parameters
     byebug
 
