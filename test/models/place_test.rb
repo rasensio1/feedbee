f9a3e209b490ff01f08efa8abec1620623b57d75
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-09 16:54:55 -0700

    add validitiy test for place

--- test/models/place_test.rb
+++ test/models/place_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class PlaceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid" do
+    place = Place.create(place_id: "abc123", 
+                 name: "first one", 
+                 image_url: "www.yeah.com", 
+                 rating: "3.5", 
+                 phone_no: "(539) 374-1838", 
+                 website: "www.ohyeah.com",
+                 hours: "big formatting stuff")
+
+    assert place.valid?
+  end
 end
