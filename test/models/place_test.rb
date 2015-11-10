1192340776c01fcaa0cd7f66474d9440a5784d77
Ryan Asensio <Ryan.asensio@gmail.com>
2015-11-10 13:45:50 -0700

    Update place_test.rb

--- test/models/place_test.rb
+++ test/models/place_test.rb
@@ -1,13 +1,12 @@
 require 'test_helper'
-
 class PlaceTest < ActiveSupport::TestCase
 
-  def new_place 
-    Place.new(place_id: "abc123", 
-                 name: "first one", 
-                 image_url: "www.yeah.com", 
-                 rating: "3.5", 
-                 phone_no: "(539) 374-1838", 
+  def new_place
+    Place.new(place_id: "abc123",
+                 name: "first one",
+                 image_url: "www.yeah.com",
+                 rating: "3.5",
+                 phone_no: "(539) 374-1838",
                  website: "www.ohyeah.com",
                  hours: "big formatting stuff")
 
