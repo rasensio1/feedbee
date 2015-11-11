6c739a0e00e629a205838a685acd6498fac76fa5
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-11 15:02:38 -0700

    add tests for comment model

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid" do
+    assert create_comment.valid?
+  end
+
+  test "is invalid without body" do
+    comment = create_comment
+    comment.body = nil
+    refute comment.valid?
+  end
+
+  test "is invalid without sentiment" do
+    comment = create_comment
+    comment.sentiment = nil
+    refute comment.valid?
+  end
+
+  test "is valid is invalid without followable_id" do
+    comment = create_comment
+    comment.followable_id = nil
+    refute comment.valid?
+  end
+
+  test "is valid is invalid without followable_type" do
+    comment = create_comment
+    comment.followable_type = nil
+    refute comment.valid?
+  end
+
+  def create_comment
+    Comment.new(body: "IM THE BODY", 
+                sentiment: 0, 
+                followable_id: 1, 
+                followable_type: "Place")
+  end
 end
