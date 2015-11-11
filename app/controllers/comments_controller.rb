f06249ac7ebad3c847de6334c0c67b151fdb0503
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-11 14:53:31 -0700

    fill in comments create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,8 @@
 class CommentsController < ApplicationController
   def create
+    comment = Comment.new(comment_params)
+    comment.save
+    flash[:success] = "Comment Saved"
     redirect_to :back
   end
 end
