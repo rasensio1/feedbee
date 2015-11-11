332f2d9d9d413e98f62e4d20637bf13ff858ac4d
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-11 16:42:04 -0700

    remove debugger

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,6 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.new(comment_params)
     comment.save
-    byebug
     flash[:success] = "Comment Saved"
     redirect_to :back
   end
