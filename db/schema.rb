72d1db3f3708bc8e43e9a3d2823a49b77744c21a
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-11 19:38:58 -0700

    add vote model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151111215500) do
+ActiveRecord::Schema.define(version: 20151112023808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,5 +73,18 @@ ActiveRecord::Schema.define(version: 20151111215500) do
     t.string   "oauth_token"
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "comment_id"
+    t.integer  "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["comment_id"], name: "index_votes_on_comment_id", using: :btree
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id", using: :btree
+
   add_foreign_key "addresses", "places"
+  add_foreign_key "votes", "comments"
+  add_foreign_key "votes", "users"
 end
