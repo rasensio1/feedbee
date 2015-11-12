849d6295ca26856e4974c2be41207e7ade190f13
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-11 20:56:49 -0700

    change upvote and downvote to class from id to enable multiple selection

---
+++ app/assets/javascripts/vote.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+
+  $( ".upvote" ).click(function() {
+    var commentId = $(this).parent().attr("id");
+    $.ajax({
+      method: "POST",
+      url: "/votes",
+      data: { value: 1,
+              comment_id: commentId },
+    });
+  });
+
+});
+
+
+
