cc1110605d31e957a70496374e7bed2d689a17b8
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-12 14:19:39 -0700

    rspec follow test passing

--- spec/features/user_follows_place_spec.rb
+++ spec/features/user_follows_place_spec.rb
@@ -4,10 +4,11 @@ RSpec.feature 'following a place' do
 
   describe "a user", :js => true do
     before(:each) do
-      stub_omniauth
       create_place
       create_user
       UserFollowsController.any_instance.stub(:current_user).
+        and_return(User.first)
+      UsersController.any_instance.stub(:current_user).
         and_return(User.first)
       CommentsController.any_instance.stub(:current_place).
         and_return(Place.first)
@@ -15,7 +16,6 @@ RSpec.feature 'following a place' do
 
     it "follows a place" do
       VCR.use_cassette("follow") do
-        login_user
         visit '/'
         visit '/places/turing-school-of-software-design'
 
@@ -23,7 +23,7 @@ RSpec.feature 'following a place' do
         expect(page).to have_content("Following")
 
         visit profile_path
-        assert page.has_content?("Turing School")
+        assert page.has_content?("turing school")
       end
     end
   end
