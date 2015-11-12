ac230d2e7ed121902eb874a6edcb9744eaeee662
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-11 20:07:37 -0700

    add vote limit per person

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,6 +3,9 @@ class Vote < ActiveRecord::Base
   belongs_to :comment
 
   validates :user_id, :comment_id, :value, presence: true
+
+  validates :user_id, uniqueness: { scope: :comment_id,
+        message: "You are already voted on this, YO" }
   validate :abs_value_one
 
   def abs_value_one
