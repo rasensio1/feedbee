49151dababa01103db831d7b9722b171b1e07fae
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-11 19:42:26 -0700

    add model tests for vote

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class VoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid" do
+    assert new_vote.valid?
+  end
+
+  test "is invalid without user_id" do
+    vote = new_vote
+    vote.user_id = nil
+
+    refute vote.valid?
+  end
+
+  test "is invalid without comment_id" do
+    vote = new_vote
+    vote.comment_id = nil
+
+    refute vote.valid?
+  end
+
+  test "is invalid without value" do
+    vote = new_vote
+    vote.value = nil
+
+    refute vote.valid?
+  end
+
+  test "is can have value as 1 or -1" do
+    vote = new_vote
+
+    vote.value = 2
+    refute vote.valid?
+
+    vote.value = -2
+    refute vote.valid?
+  end
+
+  def new_vote
+    Vote.new(user_id: 1,
+             comment_id: 2,
+             value: 1)
+  end
 end
