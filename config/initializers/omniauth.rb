1bb43002cd2836133415ee90181f11d60b2808c7
Ryan Asensio <Ryan.asensio@gmail.com>
2015-11-14 23:09:16 -0700

    remove unused params

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
             scope: 'public_profile,email', 
-            info_fields: 'id,name,link,email,location,significant_other,hometown'
+            info_fields: 'id,name,link,email'
 end
