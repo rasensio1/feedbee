2f0afd5f0270d6f01b3d2c789e13657307760e45
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-16 12:30:11 -0700

    displays search text on page for multiple searches

--- app/controllers/google/search_controller.rb
+++ app/controllers/google/search_controller.rb
@@ -1,5 +1,6 @@
 class Google::SearchController < ApplicationController
   def index
+    @search_text = params[:search_text]
     @places = params[:searches].map do |id|
       Place.for_id(id)
     end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,7 +5,7 @@ class PlacesController < ApplicationController
       if one_result?(col_place_ids)
         redirect_to place_path(slug: Place.slug_for_show(col_place_ids.first))
       else
-        redirect_to search_path(searches: col_place_ids)
+        redirect_to search_path(searches: col_place_ids, search_text: params[:go_to])
       end
     else
       flash['message'] = "Sorry, no results for #{params[:go_to]}. Try something else!"
