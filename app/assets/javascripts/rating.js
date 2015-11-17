b905d226751c03505ccf7793ccd53bc0cc4bd1d3
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-16 18:49:15 -0700

    remove rating functionality. only render picture if exists. fixes popup for checking good or bad for a comment

--- app/assets/javascripts/rating.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function() {
-  var myRating = $('.rating').attr("data-rating");
-  $('.ui.rating')
-    .rating({
-      initialRating: myRating,
-      maxRating: 5
-    });
-});
-
