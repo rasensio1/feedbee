f6660cd5ba3c399de03ff21b8827f324c3b78be2
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-16 22:37:21 -0700

    render flash message for successfull comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,12 @@ class CommentsController < ApplicationController
 
   def create
     CommentCreator.go(comment_params, current_user)
-    flash[:success] = "Comment Saved"
+    flash["message"] = "Comment Saved!"
     redirect_to :back
+  end
+
+  def destory
+    CommentCreator.destory_last_comment(current_user)
   end
 
   def index
