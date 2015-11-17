5bc1b69336f720212782aea08f4962ab11735bc7
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-16 22:38:34 -0700

    add method for destroying last comment and vote

--- app/models/comment_creator.rb
+++ app/models/comment_creator.rb
@@ -4,7 +4,13 @@ class CommentCreator
     create_dependencies(comment, current_user)
   end
 
-  def self.create_dependencies(comment, current_user)
-    Vote.create(user_id: current_user.id, comment_id: comment.id, value: 1)
+  def self.create_dependencies(comment, user)
+    Vote.create(user_id: user.id, comment_id: comment.id, value: 1)
+  end
+
+  def self.destory_last_comment(user)
+    vote = Vote.where(user_id: user.id).order(created_at: :desc).limit(1)
+    Comment.find(vote.id).destory_all
+    vote.destory_all
   end
 end
