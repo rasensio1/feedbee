406a10b5d837f8b5c0b7d8a3388c47360dc5fb1e
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-17 08:57:42 -0700

    change undo link to be present on page

--- app/assets/javascripts/undo_comment.js
+++ app/assets/javascripts/undo_comment.js
@@ -1,6 +1,6 @@
 $( document ).ready(function() {
   if ( $('#the-message').text() == "Comment Saved!") {
-    $('#the-message').parent()
-                     .append("<h3><a href='/comments/destroy'>Undo?</a></h3>")
+    $('#new-comment').parent()
+                     .append("<h3><a href='/comments/destroy'>Undo last comment?</a></h3>")
   }
 });
