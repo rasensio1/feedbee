cff7880326453ea5c4e08880e44485f79e29f7de
Ryan Asensio <ryan.asensio@gmail.com>
2015-11-18 20:46:48 -0700

    add test for omniauth failure

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+
 RSpec.feature 'logging in and out' do
   it "works" do
     stub_omniauth
@@ -6,6 +8,22 @@ RSpec.feature 'logging in and out' do
     assert page.has_content?("Ryan Asensio")
 
     click_link "Log Out"
+    assert_equal '/', current_path
+    refute page.has_content?("Ryan Asensio")
+  end
+
+  it "renders error with bad response", js: true do
+    UserFollowsController.any_instance.stub(:current_user).
+      and_return(User.first)
+    PlacesController.any_instance.stub(:current_place).
+      and_return(Place.first)
+    UsersController.any_instance.stub(:current_user).
+      and_return(User.first)
+    Comment.stub(:for_a_place).
+      and_return(nil)
+    FollowManager.stub(:is_following?).and_return(true)
+    stub_bad_omniauth
+    login_user
     assert_equal '/', current_path
     refute page.has_content?("Ryan Asensio")
   end
